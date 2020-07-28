1f00414e2d50593e11100b0a4d00b327cdb57695
Matthew Barnes <taking1n1@gmail.com>
2020-07-28 22:44:33 +0100

    Started test cases

--- src/test/soup_parser_test.py
+++ src/test/soup_parser_test.py
@@ -1,10 +1,27 @@
 import unittest
+from bs4 import BeautifulSoup
+from soup_parser import parse_soup
 
 
 class SoupParserTest(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_avenger_knight(self):
+        # Reads avenger knight html
+        file = open("html_files/avenger_knight.html", mode='r', encoding='utf-8')
+        html = file.read()
+        file.close()
 
+        # Parses html
+        soup = BeautifulSoup(html, 'html.parser')
+        parsed_result = parse_soup(soup)
+
+        # Tests variations
+        variations = map(lambda result: result[0], parsed_result)
+        self.assertIn("Persona 3 - The Journey", variations)
+        self.assertIn("Persona 3 - The Answer", variations)
+        self.assertIn("Persona 4 - Boss", variations)
+        self.assertIn("Persona 4 - Normal Enemy", variations)
+        self.assertIn("Persona 4 Golden - Boss", variations)
+        self.assertIn("Persona 4 Golden - Normal Enemy", variations)
 
 if __name__ == '__main__':
     unittest.main()
